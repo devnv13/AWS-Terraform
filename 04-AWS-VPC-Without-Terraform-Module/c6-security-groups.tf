cf698ca0f6a22b257651f1a9b218b0eb00902196
vasu <vasu@localhost.localdomain>
2024-09-07 13:52:00 +0530

    fixed

--- 04-AWS-VPC-Without-Terraform-Module/c6-security-groups.tf
+++ 04-AWS-VPC-Without-Terraform-Module/c6-security-groups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "vpc-web-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["aws_vpc.my-vpc.cidr_block"]
+    cidr_blocks = ["aws_vpc.my-vpc.cidr_block.id"]
   }
   ingress {
     description = "Allow port 80"
@@ -41,7 +41,7 @@ resource "aws_security_group" "vpc-mysql-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["aws_vpc.my-vpc.cidr_block"]
+    cidr_blocks = ["aws_vpc.my-vpc.cidr_block.id"]
   }
   ingress {
     description = "Allow port 3306"
